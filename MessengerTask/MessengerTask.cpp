95fadf16006e46ebbea7191b3c23efd33e24dd03
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-08-25 14:35:46 +0500

    Add project files.

---
+++ MessengerTask/MessengerTask.cpp
@@ -0,0 +1,12 @@
+﻿// MessengerTask.cpp : Defines the entry point for the application.
+//
+
+#include "MessengerTask.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
