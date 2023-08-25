fda8e566b82ae1bd44c6a0ead275ce848dfd0755
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-08-25 15:27:37 +0500

    Add Catch2 framework for testing

--- MessengerTask/TASK1_MESSENGER.cpp
+++ MessengerTask/TASK1_MESSENGER.cpp
@@ -1,13 +1,3 @@
 ﻿// task1_messenger.cpp : Defines the entry point for the application.
 //
-#include <iostream>
-
 #include "task1_messenger.hpp"
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello Task1." << endl;
-	return 0;
-}
