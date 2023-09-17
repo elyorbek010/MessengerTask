d48db4743d236dfa79e1a04ac669c670c6aa167b
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-09-17 05:22:24 +0500

    refactor: modify parse_buff

--- MessengerTask/src/task1_messenger.cpp
+++ MessengerTask/src/task1_messenger.cpp
@@ -102,10 +102,10 @@ std::vector<uint8_t> messenger::make_buff(const messenger::msg_t& msg)
 	return buff;
 }
 
-static void unpack_header(uint8_t high_byte, uint8_t low_byte, uint8_t& flag, uint8_t& namelen, uint8_t& textlen, uint8_t& crc) 
+static void unpack_header(std::vector<uint8_t>::const_iterator header_iter, uint8_t& flag, uint8_t& namelen, uint8_t& textlen, uint8_t& crc)
 {
-	uint16_t header = (static_cast<unsigned short>(high_byte) << 8) + (static_cast<unsigned short>(low_byte));
-
+	uint16_t header = (static_cast<unsigned short>(*header_iter) << 8) + (static_cast<unsigned short>(*(header_iter+1)));
+	
 	crc = header & N_BIT_MASK(CRC_LEN);
 	header >>= CRC_LEN;
 
@@ -119,55 +119,50 @@ static void unpack_header(uint8_t high_byte, uint8_t low_byte, uint8_t& flag, ui
 	header >>= FLAG_LEN;
 }
 
-messenger::msg_t messenger::parse_buff(std::vector<uint8_t>& buff) 
+static inline void clear_crc(std::vector<uint8_t>::iterator header_iter) 
+{
+	*(header_iter + 1) &= 0xf0; // clear the crc value of the header
+}
+
+messenger::msg_t messenger::parse_buff(std::vector<uint8_t>& buff)
 {
 	uint8_t flag(0);
 	uint8_t namelen(0);
 	uint8_t textlen(0);
 	uint8_t header_crc(0);
 	uint8_t calculated_crc(0);
-	uint8_t buff_idx(0);
-
-	size_t buff_size = buff.size();
-	if(buff_size < HEADER_SIZE)
-		throw std::runtime_error("error: invalid buffer");
-
-	unpack_header(buff[0], buff[1], flag, namelen, textlen, header_crc);
-
-	if (namelen == 0)
-	{
-		throw std::length_error("error: name is empty");
-	}
-	else if (textlen == 0) 
-	{
-		throw std::length_error("error: text is empty");
-	}
-	
+	uint8_t max_packet_len(0);
+	std::vector<uint8_t>::iterator buff_iter;
 	messenger::msg_t msg("", "");
 
-	for (size_t i = 0; i < namelen; i++)
-		msg.name += buff[HEADER_SIZE + i];
+	unpack_header(buff.begin(), flag, namelen, textlen, header_crc);
+	max_packet_len = HEADER_SIZE + namelen + MAX_MSG_LEN;
 
-	while (buff.size() > buff_idx) 
+	// Read first packet and assign NAME to msg.name
+	msg.name.assign(buff.begin() + HEADER_SIZE, buff.begin() + HEADER_SIZE + namelen); 
+
+	// Read packets and append messages to msg.text
+	for (size_t packet_n = 0; packet_n < buff.size() / max_packet_len + 1; packet_n++)
 	{
-		unpack_header(buff[buff_idx], buff[buff_idx + 1], flag, namelen, textlen, header_crc);
+		buff_iter = buff.begin() + packet_n * max_packet_len; // put iterator to the beginning of the n'th packet
+
+		unpack_header(buff_iter, flag, namelen, textlen, header_crc);
 
 		if (flag != FLAG_VAL)
 			throw std::runtime_error("error: invalid flag");
 
-		buff[buff_idx + 1] &= 0xf0; // clear the crc value in the buffer header
+		clear_crc(buff_iter);
 
-		calculated_crc = CRC::Calculate(static_cast<void*>(&(buff[buff_idx])), 
-										HEADER_SIZE + namelen + textlen, 
-										CRC::CRC_4_ITU());
+		calculated_crc = CRC::Calculate(static_cast<void*>(&*buff_iter),
+			HEADER_SIZE + namelen + textlen,
+			CRC::CRC_4_ITU());
 
 		if (header_crc != calculated_crc)
 			throw std::runtime_error("error: invalid crc");
 
-		for (size_t i = 0; i < textlen; i++)
-			msg.text += buff[buff_idx + HEADER_SIZE + namelen + i];
+		buff_iter += HEADER_SIZE + namelen; // skip to text field
 
-		buff_idx += MAX_MSG_LEN + namelen + HEADER_SIZE;
+		msg.text.append(buff_iter, buff_iter + textlen); // append the text
 	}
 
 	return msg;
