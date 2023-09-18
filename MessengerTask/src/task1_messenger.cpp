cfeb87254ccb05268a61738d14259fc5e4e9aa18
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-09-18 14:31:14 +0500

    refactor: make code clearer

--- MessengerTask/src/task1_messenger.cpp
+++ MessengerTask/src/task1_messenger.cpp
@@ -1,12 +1,12 @@
 ﻿// task1_messenger.cpp : Defines the entry point for the application.
 //
-#include <bitset>
 #include <cassert>
-
 #include <iostream>
-#define CRCPP_INCLUDE_ESOTERIC_CRC_DEFINITIONS
+#include <algorithm>
 
 #include "task1_messenger.hpp"
+
+#define CRCPP_INCLUDE_ESOTERIC_CRC_DEFINITIONS
 #include "CRC.h"
 
 #define FLAG_LEN (3)		// in bits
@@ -18,7 +18,7 @@
 #define TEXTLEN_LEN (5)		// in bits
 #define MAX_MSG_LEN (31)	// in bytes
 
-#define CRC_LEN (4)		// in bits
+#define CRC_LEN (4)			// in bits
 #define CRC_MASK (0b1111)
 #define CRC_PLACEHOLDER (0b0000)
 
@@ -49,54 +49,53 @@ static uint16_t pack_header(uint8_t flag, uint8_t namelen, uint8_t textlen, uint
 	return header;
 }
 
-std::vector<uint8_t> messenger::make_buff(const messenger::msg_t& msg) 
+std::vector<uint8_t> messenger::make_buff(const messenger::msg_t& msg)
 {
-	std::string::size_type namelen = msg.name.length();
-	std::string::size_type textlen = msg.text.length();
+	std::string::const_iterator name_iter = msg.name.begin();
+	std::string::const_iterator text_iter = msg.text.begin();
 
-	if (namelen == 0) 
+	uint8_t namelen = msg.name.length();
+	uint8_t textlen = msg.text.length();
+
+	if (namelen == 0)
 	{
 		throw std::length_error("error: name is empty");
 	}
-	else if (textlen == 0) 
+	else if (textlen == 0)
 	{
 		throw std::length_error("error: text is empty");
 	}
-	else if (namelen > 15) 
+	else if (namelen > 15)
 	{
 		throw std::length_error("error: name is too long");
 	}
 
 	std::vector<uint8_t> buff;
-	std::string::size_type cur_textlen(0);
+	uint8_t cur_textlen(0);
+	uint8_t text_pos(0);
 	uint16_t header(0);
-	uint8_t packet_n(0);
 	uint8_t crc(0);
 
-	while (textlen > 0) 
+	uint8_t max_packet_len = HEADER_SIZE + namelen + MAX_MSG_LEN;
+	uint8_t num_of_packets = textlen / MAX_MSG_LEN + (textlen % MAX_MSG_LEN != 0); // if textlen % max_msg_len != 0, there is an additional packet
+
+	for (uint8_t packet_n = 0; packet_n < num_of_packets; packet_n++, textlen -= MAX_MSG_LEN, text_iter += cur_textlen)
 	{
-		cur_textlen = textlen > MAX_MSG_LEN ? MAX_MSG_LEN : textlen;
-		textlen -= cur_textlen;
+		cur_textlen = std::min((uint8_t)textlen, (uint8_t)MAX_MSG_LEN);
 
 		header = pack_header(FLAG_VAL, namelen, cur_textlen, CRC_PLACEHOLDER);
 
-		buff.push_back(static_cast<uint8_t>(header >> 8));		// higher byte of header
-		buff.push_back(static_cast<uint8_t>(header & 0x00ff));	// lower byte of header
+		buff.push_back(static_cast<uint8_t>(header >> 8));			// higher byte of header
+		buff.push_back(static_cast<uint8_t>(header & 0x00ff));		// lower byte of header
 
-		for (size_t i = 0; i < namelen; i++)
-			buff.push_back(msg.name[i]);
+		buff.insert(buff.end(), name_iter, name_iter + namelen);	// insert name field
+		buff.insert(buff.end(), text_iter, text_iter + cur_textlen);// insert text field
 
-		for (size_t i = 0; i < cur_textlen; i++)
-			buff.push_back(msg.text[packet_n * MAX_MSG_LEN + i]);
+		crc = CRC::Calculate(static_cast<void*>(&buff[packet_n * max_packet_len]),
+			HEADER_SIZE + namelen + cur_textlen,
+			CRC::CRC_4_ITU());
 
-		crc = CRC::Calculate(static_cast<void*>(&buff[packet_n * (HEADER_SIZE + namelen + MAX_MSG_LEN)]),
-							HEADER_SIZE + namelen + cur_textlen, 
-							CRC::CRC_4_ITU());
-		
-		buff[packet_n * (MAX_MSG_LEN + namelen + HEADER_SIZE) + 1] &= 0xf0; // clear the crc field
-		buff[packet_n * (MAX_MSG_LEN + namelen + HEADER_SIZE) + 1] |= crc;  // set the crc value
-
-		packet_n++;
+		buff[packet_n * max_packet_len + 1] |= crc;  // set the crc value
 	}
 
 	return buff;
@@ -119,9 +118,9 @@ static void unpack_header(std::vector<uint8_t>::const_iterator header_iter, uint
 	header >>= FLAG_LEN;
 }
 
-static inline void clear_crc(std::vector<uint8_t>::iterator header_iter) 
+static inline void clear_crc(std::vector<uint8_t>::iterator buff_header) 
 {
-	*(header_iter + 1) &= 0xf0; // clear the crc value of the header
+	*(buff_header + 1) &= 0xf0; // clear the crc value of the header
 }
 
 messenger::msg_t messenger::parse_buff(std::vector<uint8_t>& buff)
@@ -132,7 +131,7 @@ messenger::msg_t messenger::parse_buff(std::vector<uint8_t>& buff)
 	uint8_t header_crc(0);
 	uint8_t calculated_crc(0);
 	uint8_t max_packet_len(0);
-	std::vector<uint8_t>::iterator buff_iter;
+	std::vector<uint8_t>::iterator buff_iter = buff.begin();
 	messenger::msg_t msg("", "");
 
 	unpack_header(buff.begin(), flag, namelen, textlen, header_crc);
@@ -144,8 +143,6 @@ messenger::msg_t messenger::parse_buff(std::vector<uint8_t>& buff)
 	// Read packets and append messages to msg.text
 	for (size_t packet_n = 0; packet_n < buff.size() / max_packet_len + 1; packet_n++)
 	{
-		buff_iter = buff.begin() + packet_n * max_packet_len; // put iterator to the beginning of the n'th packet
-
 		unpack_header(buff_iter, flag, namelen, textlen, header_crc);
 
 		if (flag != FLAG_VAL)
@@ -163,6 +160,8 @@ messenger::msg_t messenger::parse_buff(std::vector<uint8_t>& buff)
 		buff_iter += HEADER_SIZE + namelen; // skip to text field
 
 		msg.text.append(buff_iter, buff_iter + textlen); // append the text
+
+		buff_iter += textlen;
 	}
 
 	return msg;
