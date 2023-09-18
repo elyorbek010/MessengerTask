6d6a7126ffba86a43716c508bbcb045ff24ac66f
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-09-18 17:00:36 +0500

    test: feat: Add new tests

--- MessengerTask/test/messenger_test.cpp
+++ MessengerTask/test/messenger_test.cpp
@@ -7,49 +7,268 @@
 
 #include "task1_messenger.hpp"
 
-#define HEADER_SIZE (2)
-#define MAX_MESSAGE_SIZE (31)
+#define CRCPP_INCLUDE_ESOTERIC_CRC_DEFINITIONS
+#include "CRC.h"
 
-TEST_CASE("EmptyName", "MakeBuff") {
+#define FLAG_LEN (3)		// in bits
+#define FLAG_VAL (0b101)
+
+#define NAMELEN_LEN (4)		// in bits
+#define MAX_NAME_LEN (15)	// in bytes
+
+#define TEXTLEN_LEN (5)		// in bits
+#define MAX_MSG_LEN (31)	// in bytes
+
+#define CRC_LEN (4)			// in bits
+#define CRC_MASK (0b1111)
+#define CRC_PLACEHOLDER (0b0000)
+
+#define HEADER_SIZE (2)		// in bytes
+
+#define N_BIT_MASK(num) (0xffff >> (16 - num))
+
+TEST_CASE("MakeBuff_NameLen0", "MakeBuff") {
 	std::string name("");
-	std::string message("Hi");
+	std::string text("Hi");
+
+	bool caught_error = false;
+
+	try
+	{
+		const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
+	}
+	catch (const std::length_error& error) {
+		caught_error = true;
+	}
+
+	REQUIRE(caught_error == true);
+}
+
+TEST_CASE("MakeBuff_NameLen1", "MakeBuff") {
+	std::string name("E");
+	std::string text("Hi");
+
+	std::vector<uint8_t> buff1{ 0b10100010, 0b00100000 };// insert header
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin(), text.end()); // insert text
+
+	uint8_t crc = CRC::Calculate(static_cast<void*>(&buff1[0]), // calculate crc value
+		HEADER_SIZE + name.size() + text.size(),
+		CRC::CRC_4_ITU());
+
+	buff1[1] |= crc; // set crc value
+
+	const std::vector<uint8_t>& buff2 = messenger::make_buff(messenger::msg_t(name, text)); // get buffer from make_buff
+
+	REQUIRE(buff1 == buff2);
+}
+
+TEST_CASE("MakeBuff_NameLen15", "MakeBuff") {
+	std::string name("ElyorbekElyorbe");
+	std::string text("Hi");
+
+	std::vector<uint8_t> buff1{ 0b10111110, 0b00100000 };// insert header
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin(), text.end()); // insert text
+
+	uint8_t crc = CRC::Calculate(static_cast<void*>(&buff1[0]), // calculate crc value
+		HEADER_SIZE + name.size() + text.size(),
+		CRC::CRC_4_ITU());
+
+	buff1[1] |= crc; // set crc value
+
+	const std::vector<uint8_t>& buff2 = messenger::make_buff(messenger::msg_t(name, text)); // get buffer from make_buff
+
+	REQUIRE(buff1 == buff2);
+}
+
+TEST_CASE("MakeBuff_NameLen16", "MakeBuff") {
+	std::string name("ElyorbekElyorbek");
+	std::string text("Hi");
+
+	bool caught_error = false;
+
+	try
+	{
+		const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
+	}
+	catch (const std::length_error& error) {
+		caught_error = true;
+	}
+
+	REQUIRE(caught_error == true);
+}
+
+TEST_CASE("MakeBuff_MsgLen0", "MakeBuff") {
+	std::string name("Elyorbek");
+	std::string text("");
+
+	bool caught_error = false;
+
+	try
+	{
+		const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
+	}
+	catch (const std::length_error& error) {
+		caught_error = true;
+	}
+
+	REQUIRE(caught_error == true);
+}
+
+TEST_CASE("MakeBuff_MsgLen1", "MakeBuff") {
+	std::string name("Elyorbek");
+	std::string text("H");
+
+	std::vector<uint8_t> buff1{ 0b10110000, 0b00010000 };// insert header
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin(), text.end()); // insert text
+
+	uint8_t crc = CRC::Calculate(static_cast<void*>(&buff1[0]), // calculate crc value
+		HEADER_SIZE + name.size() + text.size(),
+		CRC::CRC_4_ITU());
+
+	buff1[1] |= crc; // set crc value
+
+	const std::vector<uint8_t>& buff2 = messenger::make_buff(messenger::msg_t(name, text)); // get buffer from make_buff
+
+	REQUIRE(buff1 == buff2);
+}
+
+TEST_CASE("MakeBuff_MsgLen31", "MakeBuff") {
+	std::string name("Elyorbek");
+	std::string text("this message contains 31 chars ");
+
+	std::vector<uint8_t> buff1{ 0b10110001, 0b11110000 };// insert header
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin(), text.end()); // insert text
+
+	uint8_t crc = CRC::Calculate(static_cast<void*>(&buff1[0]), // calculate crc value
+		HEADER_SIZE + name.size() + text.size(),
+		CRC::CRC_4_ITU());
+
+	buff1[1] |= crc; // set crc value
+
+	const std::vector<uint8_t>& buff2 = messenger::make_buff(messenger::msg_t(name, text)); // get buffer from make_buff
+
+	REQUIRE(buff1 == buff2);
+}
+
+TEST_CASE("MakeBuff_1PacketMaxNameMaxMsg", "MakeBuff") {
+	std::string name("ElyorbekElyorbe");
+	std::string text("this message contains 31 chars ");
+
+	std::vector<uint8_t> buff1{ 0b10111111, 0b11110000 };// insert header
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin(), text.end()); // insert text
+
+	uint8_t crc = CRC::Calculate(static_cast<void*>(&buff1[0]), // calculate crc value
+		HEADER_SIZE + name.size() + text.size(),
+		CRC::CRC_4_ITU());
+
+	buff1[1] |= crc; // set crc value
+
+	const std::vector<uint8_t>& buff2 = messenger::make_buff(messenger::msg_t(name, text)); // get buffer from make_buff
+
+	REQUIRE(buff1 == buff2);
+}
+
+TEST_CASE("MakeBuff_MsgLen32", "MakeBuff") {
+	std::string name("Elyorbek");
+	std::string text("this message contains 32 chars  ");
+
+	uint8_t header1_h = 0b10110001;
+	uint8_t header1_l = 0b11110000;
+
+	uint8_t header2_h = 0b10110000;
+	uint8_t header2_l = 0b00010000;
+
+	std::vector<uint8_t> buff1;
+	buff1.push_back(header1_h);	// insert header
+	buff1.push_back(header1_l);
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin(), text.begin() + MAX_MSG_LEN); // insert text
+
+	buff1.push_back(header2_h);	// insert header
+	buff1.push_back(header2_l);
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin() + MAX_MSG_LEN, text.end()); // insert text
+
+	uint8_t crc = CRC::Calculate(static_cast<void*>(&buff1[0]), // calculate crc value
+		HEADER_SIZE + name.size() + MAX_MSG_LEN,
+		CRC::CRC_4_ITU());
+
+	buff1[1] |= crc; // set crc value
+
+	crc = CRC::Calculate(static_cast<void*>(&buff1[HEADER_SIZE + name.size() + MAX_MSG_LEN]), // calculate crc value
+		HEADER_SIZE + name.size() + 1,
+		CRC::CRC_4_ITU());
+
+	buff1[HEADER_SIZE + name.size() + MAX_MSG_LEN + 1] |= crc; // set crc value
+
+	const std::vector<uint8_t>& buff2 = messenger::make_buff(messenger::msg_t(name, text)); // get buffer from make_buff
+
+	REQUIRE(buff1 == buff2);
+}
+
+TEST_CASE("MakeBuff_MsgLen62", "MakeBuff") {
+	std::string name("Elyorbek");
+	std::string text("this message contains 62 chars,this message contains 62 chars ");
+
+	uint8_t header_h = 0b10110001;
+	uint8_t header_l = 0b11110000;
+
+	std::vector<uint8_t> buff1;
+	buff1.push_back(header_h);	// insert header
+	buff1.push_back(header_l);
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin(), text.begin() + MAX_MSG_LEN); // insert text
+
+	buff1.push_back(header_h);	// insert header
+	buff1.push_back(header_l);
+	buff1.insert(buff1.end(), name.begin(), name.end()); // insert name
+	buff1.insert(buff1.end(), text.begin() + MAX_MSG_LEN, text.end()); // insert text
+
+	uint8_t crc = CRC::Calculate(static_cast<void*>(&buff1[0]), // calculate crc value
+		HEADER_SIZE + name.size() + MAX_MSG_LEN,
+		CRC::CRC_4_ITU());
+
+	buff1[1] |= crc; // set crc value
+
+	crc = CRC::Calculate(static_cast<void*>(&buff1[HEADER_SIZE + name.size() + MAX_MSG_LEN]), // calculate crc value
+		HEADER_SIZE + name.size() + MAX_MSG_LEN,
+		CRC::CRC_4_ITU());
+
+	buff1[HEADER_SIZE + name.size() + MAX_MSG_LEN + 1] |= crc; // set crc value
+
+	const std::vector<uint8_t>& buff2 = messenger::make_buff(messenger::msg_t(name, text)); // get buffer from make_buff
+	
+	REQUIRE(buff1 == buff2);
+}
+
+TEST_CASE("ParseBuff_WrongFlag", "ParseBuff") {
+	std::string name("Elyorbek");
+	std::string text("Hi");
+
+	std::vector<uint8_t> buff = messenger::make_buff(messenger::msg_t(name, text));
+
+	buff[0] &= 0x1f; // clear flag field
+
+	bool caught_error = false;
 
 	try 
 	{
-		const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, message));
+		const messenger::msg_t& text = messenger::parse_buff(buff);
 	}
-	catch (const std::length_error& error) {
-		std::cout << error.what() << std::endl;
-	}
-}
-
-TEST_CASE("EmptyMessage", "MakeBuff") {
-	std::string name("Elyorbek");
-	std::string message("");
-
-	try
+	catch (const std::runtime_error& error) 
 	{
-		const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, message));
+		caught_error = true;
 	}
-	catch (const std::length_error& error) {
-		std::cout << error.what() << std::endl;
-	}
+
+	REQUIRE(caught_error == true);
 }
 
-TEST_CASE("NameLimit", "MakeBuff") {
-	std::string name("ElyorbekElyorbek");
-	std::string message("Hi");
-
-	try
-	{
-		const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, message));
-	}
-	catch (const std::length_error& error) {
-		std::cout << error.what() << std::endl;
-	}
-}
-
-TEST_CASE("CRCCheck", "ParseBuff") {
+TEST_CASE("ParseBuff_WrongCRC", "ParseBuff") {
 	std::string name("Elyorbek");
 	std::string text("Hi");
 
@@ -58,88 +277,33 @@ TEST_CASE("CRCCheck", "ParseBuff") {
 
 	buff_cpy[1] &= 0xf0; // clear crc field
 
+	bool caught_error = false;
+
 	try {
 		const messenger::msg_t& message = messenger::parse_buff(buff_cpy);
 	}
 	catch (const std::runtime_error& error) {
-		std::cout << error.what() << std::endl;
+		caught_error = true;
 	}
+
+	REQUIRE(caught_error == true);
 }
 
-TEST_CASE("1PacketTestMakeBuff", "MakeBuff") {
-	std::string name("Elyorbek");
-	std::string message("Hi");
-
-    const std::vector<uint8_t> &buff = messenger::make_buff(messenger::msg_t(name, message));
-	// flag			5  = 0b101
-	// namelen		8  = 0b1000
-	// msglen		2  = 0b00010
-	// crc4			10 = 0b1010
-	// header 1011 0000 0010 1010
-	unsigned short correct_header = 0b1011000000101010;
-
-	unsigned short buffer_header = (static_cast<unsigned short>(buff[0]) << 8) +
-								  (static_cast<unsigned short>(buff[1]));
-
-    REQUIRE(buffer_header == correct_header);
-
-	for (size_t i = 0; i < name.size(); i++)
-		REQUIRE(buff[HEADER_SIZE + i] == name[i]);
-
-	for (size_t i = 0; i < message.size(); i++)
-		REQUIRE(buff[HEADER_SIZE + name.size() + i] == message[i]);
-}
-
-TEST_CASE("2PacketTestMakeBuff", "MakeBuff") {
-	std::string name("Elyorbek");
-	std::string text("This is a pretty long text that takes 2 packets");
-
+TEST_CASE("ParseBuff_NameLen1", "ParseBuff") {
+	std::string name("E");
+	std::string text("Hi");
+	
 	const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
-	// PACKET 1
-	// flag			5  = 0b101
-	// namelen		8  = 0b1000
-	// msglen		31 = 0b11111
-	// crc4			10 = 0b1100
-	// header 1011 0001 1111 1100
-	// 
-	// PACKET 2
-	// flag			5  = 0b101
-	// namelen		8  = 0b1000
-	// msglen		16 = 0b10000
-	// crc4			11 = 0b1011
-	// header 1011 0001 0000 1011
-	unsigned long correct_header_1 = 0b1011000111111100;
-	unsigned long correct_header_2 = 0b1011000100001011;
-
-	SECTION("Packet number 1") {
-		unsigned short buffer_header_1 = (static_cast<unsigned short>(buff[0]) << 8) +
-			(static_cast<unsigned short>(buff[1]));
-
-		REQUIRE(buffer_header_1 == correct_header_1);
-
-		for (size_t i = 0; i < name.size(); i++)
-			REQUIRE(buff[HEADER_SIZE + i] == name[i]);
-
-		for (size_t i = 0; i < MAX_MESSAGE_SIZE; i++)
-			REQUIRE(buff[HEADER_SIZE + name.size() + i] == text[i]);
-	}
-
-	SECTION("Packet number 2") {
-		unsigned short buffer_header_2 = (static_cast<unsigned short>(buff[41]) << 8) +
-			(static_cast<unsigned short>(buff[42]));
-
-		REQUIRE(buffer_header_2 == correct_header_2);
-
-		for (size_t i = 0; i < name.size(); i++)
-			REQUIRE(buff[2 * HEADER_SIZE + name.size() + MAX_MESSAGE_SIZE + i] == name[i]);
-
-		for (size_t i = 0; i < text.size() - MAX_MESSAGE_SIZE; i++)
-			REQUIRE(buff[2 * HEADER_SIZE + 2 * name.size() + MAX_MESSAGE_SIZE + i] == text[MAX_MESSAGE_SIZE + i]);
-	}
+	std::vector<uint8_t> buff_cpy = buff;
+	
+	const messenger::msg_t& message = messenger::parse_buff(buff_cpy);
+	
+	REQUIRE(message.name == name);
+	REQUIRE(message.text == text);
 }
 
-TEST_CASE("1PacketTestParseBuff", "ParseBuff") {
-	std::string name("Elyorbek");
+TEST_CASE("ParseBuff_NameLen15", "ParseBuff") {
+	std::string name("ElyorbekElyorbe");
 	std::string text("Hi");
 
 	const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
@@ -151,9 +315,62 @@ TEST_CASE("1PacketTestParseBuff", "ParseBuff") {
 	REQUIRE(message.text == text);
 }
 
-TEST_CASE("2PacketTestParseBuff", "ParseBuff") {
+TEST_CASE("ParseBuff_MsgLen1", "ParseBuff") {
 	std::string name("Elyorbek");
-	std::string text("This is a pretty long text that takes 2 packets");
+	std::string text("H");
+
+	const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
+	std::vector<uint8_t> buff_cpy = buff;
+
+	const messenger::msg_t& message = messenger::parse_buff(buff_cpy);
+
+	REQUIRE(message.name == name);
+	REQUIRE(message.text == text);
+}
+
+TEST_CASE("ParseBuff_MsgLen31", "ParseBuff") {
+	
+	std::string name("Elyorbek");
+	std::string text("this message contains 31 chars ");
+
+	const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
+	std::vector<uint8_t> buff_cpy = buff;
+
+	const messenger::msg_t& message = messenger::parse_buff(buff_cpy);
+
+	REQUIRE(message.name == name);
+	REQUIRE(message.text == text);
+}
+
+TEST_CASE("ParseBuff_1PacketMaxNameMaxMsg", "ParseBuff") {
+	std::string name("ElyorbekElyorbe");
+	std::string text("this message contains 31 chars ");
+
+	const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
+	std::vector<uint8_t> buff_cpy = buff;
+
+	const messenger::msg_t& message = messenger::parse_buff(buff_cpy);
+
+	REQUIRE(message.name == name);
+	REQUIRE(message.text == text);
+}
+
+TEST_CASE("ParseBuff_MsgLen32", "ParseBuff") {
+	std::string name("Elyorbek");
+	std::string text("this message contains 32 chars  ");
+
+	const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
+	std::vector<uint8_t> buff_cpy = buff;
+
+	const messenger::msg_t& message = messenger::parse_buff(buff_cpy);
+
+	REQUIRE(message.name == name);
+	REQUIRE(message.text == text);
+}
+
+TEST_CASE("ParseBuff_MsgLen62", "ParseBuff") {
+	std::string name("Elyorbek");
+	std::string text("this message contains 62 chars,this message contains 62 chars ");
 
 	const std::vector<uint8_t>& buff = messenger::make_buff(messenger::msg_t(name, text));
 	std::vector<uint8_t> buff_cpy = buff;
